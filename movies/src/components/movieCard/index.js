426dbaa5138a17fae9e8b3c4f3ac483af4abb50f
Alex Fan <20077347@mail.wit.ie>
2023-10-17 12:49:29 +0100

    Show movie review excerpts; Refactor Details page.

--- movies/src/components/movieCard/index.js
+++ movies/src/components/movieCard/index.js
@@ -13,6 +13,7 @@ import IconButton from "@mui/material/IconButton";
 import Grid from "@mui/material/Grid";
 import img from '../../images/film-poster-placeholder.png'
 import Avatar from '@mui/material/Avatar';
+import { Link } from "react-router-dom";
 
 export default function MovieCard(props) {
   const movie = props.movie;
@@ -64,9 +65,11 @@ export default function MovieCard(props) {
         <IconButton aria-label="add to favorites" onClick={handleAddToFavorite}>
           <FavoriteIcon color="primary" fontSize="large" />
         </IconButton>
-        <Button variant="outlined" size="medium" color="primary">
-          More Info ...
-        </Button>
+        <Link to={`/movies/${movie.id}`}>
+          <Button variant="outlined" size="medium" color="primary">
+            More Info ...
+          </Button>
+        </Link>
       </CardActions>
     </Card>
   );
